f99cc6f63ec57504f77e35abf73569a7894f6ad0
Mauro Lanza <maurolanza@hotmail.it>
2017-06-02 09:32:43 -0400

    Update youtube_multi_dl.py
    functions for less code repetition, make copies so changes during loop are not kept in the next one.

--- youtube_multi_dl.py
+++ youtube_multi_dl.py
@@ -17,61 +17,67 @@ import youtube_dl
 ######################
 
 #/////////////////////////////////////////////#
-music_options = {
+music_options_ = {
 		'writethumbnail':True,
 		'format':'bestaudio/best',
 		'extractaudio':True,
 		'audioformat':'best',
 		'outtmpl': '~/Music/Youtube_dl/%(title)s.%(ext)s',
 		'noplaylist':True,
+		'externaldownloader':'aria2c',						# fast downloader
+		'externaldownloaderargs':'-x 4 -k 1M',				# number of threads for each download and split size
 		'nocheckcertificate':True,
 		'postprocessors': [ {'key': 'FFmpegExtractAudio',	# down here is where the conversion happens,
-							'preferredcodec': 'mp3',		# preferred audio format can be changed,
-							'preferredquality': '0',},		# the quality has not much difference because
-							{'key': 'EmbedThumbnail'}]}		# the audio scrapped is already downgraded by YT
-															# did some research... not the true HD audio.
+							'preferredcodec': 'm4a',		# preferred audio format can be changed,
+							'preferredquality': '0',},		# set to convert to best, default with ffmpeg seems like 185kbps
+							{'key': 'EmbedThumbnail'}]}		
 #/////////////////////////////////////////////#
-# for now resolution is hardcoded to 720p (better for my kind of bandwidth)
-video_options = {
-		'format':'best[height<=720]',
-		'outtmpl': '~/Movies/Youtube_dl/%(title)s.%(ext)s',
+video_options_ = {
+		'format':'best[height<=720]',						# change high for better resolution
+		'outtmpl': '~/Movies/Youtube_dl/%(title)s.%(ext)s',	# download path
 		'noplaylist':True,
+		'externaldownloader':'aria2c',						# fast downloader
+		'externaldownloaderargs':'-x 4 -k 20M',				# number of threads for each download and split size
 		'nocheckcertificate':True }
 #/////////////////////////////////////////////#
+def close_scritpt_():
+	if raw_input('\nWant more?.....NO![n]\n>>> ') == 'n':
+		return True
+#/////////////////////////////////////////////#
+def download_links_(options):
+	# loads multiple links to a list 
+	links = raw_input('\nInsert link (separate multiple links by space)\n>>> ').split(" ")
+	print 'Links to download: %d' % len(links)
+	count = 1
+	# the actual music downloader
+	for link in links:
+		print '\nDownloading link %d/%d' % (count , len(links))	# links counter feedback
+		with youtube_dl.YoutubeDL(options) as ydl:
+			ydl.download([link])
+		count += 1
+#/////////////////////////////////////////////#
 def main():
-	dl =raw_input('Select type of download:.... music [m] / video [v] / quit [q]\n>>> ')
+	dl_type =raw_input('Select type of download:.... music [m] / video [v] / quit [q]\n>>> ')
 	#////////////////////#
-	while dl == 'm':	# music downloader initialization 
-
+	while dl_type == 'm':	# music downloader initialization 
+		music_options = music_options_.copy()
 		# modify some options to allow playlists and better organization of output
 		if (raw_input('\nIs Playlist?.....Yes[y]\n>>> ')) == 'y':
-			music_options['outtmpl'] = '~/Music/Youtube_dl/%(uploader)s/%(playlist)s/%(title)s.%(ext)s'
 			del music_options['noplaylist']
-
-		# loads multiple links to a list 
-		links = raw_input('\nInsert link (separate multiple links by space)\n>>> ')
-		links = links.split(" ")
-
-		print 'Files to download: %d' % len(links)
-		count = 1
-
-		# the actual music downloader
-		for link in links:
-			print '\nDownloading %d/%d' % (count , len(links))	# links counter feedback
-			with youtube_dl.YoutubeDL(music_options) as ydl:
-				ydl.download([link])
-			count += 1
-
+			music_options['outtmpl'] = '~/Music/Youtube_dl/%(uploader)s/%(playlist)s/%(title)s.%(ext)s'
+			
+		
+		download_links_(music_options)
 		# for the new wave of links
-		iterate = raw_input('\nWant more?.....NO![n]\n>>> ')
-		if iterate == 'n':
-			dl = 'q'
+		if close_scritpt_():
+			dl_type = 'q'
 			break
 	#////////////////////#
-	while dl == 'v':	# music downloader initialization 
-
+	while dl == 'v':	# video downloader initialization 
+		video_options = video_options_.copy()
 		# modify some options to allow playlists and better organization of output
 		if (raw_input('\nIs Playlist?.....Yes[y]\n>>> ')) == 'y':
+			del music_options['noplaylist']
 
 			if (raw_input('\nSave Playlist Index?.....Yes[y]\n>>> ')) == 'y':
 				music_options['outtmpl'] = '~/Movies/Youtube_dl/%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'
@@ -79,37 +85,17 @@ def main():
 			else:
 				music_options['outtmpl'] = '~/Movies/Youtube_dl/%(uploader)s/%(playlist)s/%(title)s.%(ext)s'
 
-			del music_options['noplaylist']
-
-		# loads multiple links to a list 
-		links = raw_input('\nInsert link (separate multiple links by space)\n>>> ')
-		links = links.split(" ")
-
-		print 'Files to download: %d' % len(links)
-		count = 1
-
-		# the actual video downloader
-		for link in links:
-			print '\nDownloading %d/%d' % (count , len(links))	# links counter feedback
-			with youtube_dl.YoutubeDL(video_options) as ydl:
-				ydl.download([link])
-			count += 1
-
+		download_links_(video_options)
 		# for the new wave of links
-		iterate = raw_input('\nWant more? NO![n]\n>>> ')
-		if iterate == 'n':
-			dl = 'q'
+		if close_scritpt_():
+			dl_type = 'q'
 			break
 	#////////////////////#
 	return dl
 
 ######################
-print '\n\tYouTube downloader to mp3/mp4'
-
+print '\n\tYouTube downloader to m4a/mp4'
 while __name__ == '__main__':
-
-	dl = main()
-
-	if dl == 'q':
-		break
-
+	while main() != 'q':
+		pass
+	break
