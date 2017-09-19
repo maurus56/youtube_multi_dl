c0e4797c1e518c51193d88509ed0901cd580f13a
Mauro Lanza <maurolanza@hotmail.it>
2017-09-19 17:56:27 -0400

    Update youtube_multi_dl.py

--- youtube_multi_dl.py
+++ youtube_multi_dl.py
@@ -1,14 +1,5 @@
 '''
-This is one of my first programs, the initial focus was to create a simple music/video
-downloader to satisfy personal needs. While creating it, people started asking
-for a copy so in the en I decided to make it more intuitive through the terminal.
-
-The program lacks speed so for now I'll try to implement some sort of multi threading.
-Possible GUI in the future.
-
-Lets see how further we can make this small project go. Maybe an OS multi platform app??
-
-This was mostly the easiest things I could pull out. Now the work is to level up the complexity.
+...
 '''
 
 ######################
